ff03be7789625cc9a272c0089bc50c9984764246
obijuan <obijuan.cube@gmail.com>
2025-07-06 21:09:01 +0200

    L1: update example

--- wiki/L1/Examples/hello-world-01/src/main.rs
+++ wiki/L1/Examples/hello-world-01/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hola mundo!");
+    println!(".");
+    println!("══════════════════════════════")
+    println!("Mensaje 1")
+    println!("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    println!("Mensaje 2")
+    println!("──────────────────────────────")
 }
