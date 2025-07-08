a4977f54b8e11bc71d5d20ca0593be031a269166
obijuan <obijuan.cube@gmail.com>
2025-07-08 11:54:16 +0200

    Log: add example

---
+++ wiki/Log/2025-07-08/var-07.rs
@@ -0,0 +1,13 @@
+//-- Impresión en diferentes bases
+
+fn main() {
+
+
+    let a: u8 = 0xAA;
+
+    println!("Valor decimal     : {}", a);
+    println!("Valor hexadecimal : {:#X}", a);
+    println!("Valor binario     : {:#b}", a);
+
+}
+
