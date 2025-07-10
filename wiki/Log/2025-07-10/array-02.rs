279169cfd323675fa5fa5826029a5330bebbb209
obijuan <obijuan.cube@gmail.com>
2025-07-10 12:07:40 +0200

    Log: add example

---
+++ wiki/Log/2025-07-10/array-02.rs
@@ -0,0 +1,15 @@
+//-- Pruebas con arrays
+
+fn main() {
+
+    //-- Array de 5 elementos
+    let a = [10, 20, 30, 40, 50];
+
+    //-- Imprimir el array completo
+    println!("Array: {:?}", a);
+
+    //-- Imprimir longitud del array
+    println!("Longitud: {}", a.len());
+
+}
+
