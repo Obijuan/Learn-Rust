8b6262b9148b651b2a596fc7e5ecfd7ec6842975
obijuan <obijuan.cube@gmail.com>
2025-07-10 13:27:41 +0200

    Log: add example

---
+++ wiki/Log/2025-07-10/char-03.rs
@@ -0,0 +1,21 @@
+//-- Probando caracteres unicode
+
+fn main() {
+    // Caracteres unicode
+    let char1 = 'a'; // Letra a
+    let char2 = 'ñ'; // Letra ñ
+    let char3 = 'ü'; // Letra ü
+    let char4 = '😊'; // Emoji
+    let char5 = '🟢'; 
+    let char6 = '🚀'; // Cohete
+    let char7 = '─'; 
+
+    println!("Caracter 1: {}", char1);
+    println!("Caracter 2: {}", char2);
+    println!("Caracter 3: {}", char3);
+    println!("Caracter 4: {}", char4);
+    println!("Caracter 5: {}", char5);
+    println!("Caracter 6: {}", char6);
+    println!("Caracter 7: {}", char7);
+    
+}
