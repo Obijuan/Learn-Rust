ebb42a00ac9e9e6f3d6c1cd03c1549a0982a4158
obijuan <obijuan.cube@gmail.com>
2025-07-10 14:19:21 +0200

    Log: add example

---
+++ wiki/Log/2025-07-10/cad-03.rs
@@ -0,0 +1,19 @@
+//-- Ejemplos de cadenas
+
+fn main() {
+
+    let formal = true;
+
+    //-- Cadena condicional
+
+    let saludo = if formal {
+        "Hola, ¿cómo está usted?"
+    } else {
+        "🟢¡Holi!!!🙂"
+    };
+
+
+    println!("{}", saludo);    
+
+}
+
