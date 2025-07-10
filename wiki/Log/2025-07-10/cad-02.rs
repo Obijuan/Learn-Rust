fc434378d72b3954159aeac39add7b1cc5da4b8b
obijuan <obijuan.cube@gmail.com>
2025-07-10 14:11:44 +0200

    Log: add example

---
+++ wiki/Log/2025-07-10/cad-02.rs
@@ -0,0 +1,13 @@
+//-- Ejemplos de cadenas
+
+fn main() {
+
+    let cad1  = "🟢Hola!!🙂";
+    let cad2  = "Holi!";
+
+    //-- Imprimir las cadenas
+    println!("Valor de cad1: {}", cad1);
+    println!("Valor de cad2: {}", cad2);
+
+}
+
