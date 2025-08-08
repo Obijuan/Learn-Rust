416909d0c07a489e929518b9896b49087cb01db4
obijuan <obijuan.cube@gmail.com>
2025-08-08 21:32:08 +0200

    add example

---
+++ learn-examples/src/bin/2025-08-08-ex10-option.rs
@@ -0,0 +1,26 @@
+
+//-- Recibir un valor (opcional) e imprimir su valor
+//-- Se devuelve el propio valor recibido
+//-- Es una especie de funcion identidad
+fn id(a: Option<u8>) -> Option<u8> 
+{
+    //-- Obtener el valor de a, si existe
+    //-- De lo contrario se retorna None
+    let c = a?;
+    println!("Valor: {}", c);
+
+    return Some(c);
+}
+
+fn main()
+{
+    let a: Option<u8> = Some(5);
+    let b: Option<u8> = None;
+
+    //-- Imprimir el valor de a, si existe
+    id(a);
+
+    //-- Imprimir el valor de b, si existe
+    id(b);
+
+}
