27b9a15be26e25be8c8e3da3d7a644b84d6f3570
obijuan <obijuan.cube@gmail.com>
2025-08-09 07:41:51 +0200

    add example

--- learn-examples/src/bin/2025-08-08-ex13-vec.rs
+++ learn-examples/src/bin/2025-08-08-ex13-vec.rs
@@ -3,6 +3,9 @@ fn main()
     //-- Definir una lista de 3 bytes
     let v: Vec<u8> = vec![0xff, 0xaa, 0x00];
 
+    //-- Imprimir longitud del vector
+    println!("Numero de bytes: {}", v.len());
+
     //-- Imprimir los bytes
     for byte in v {
         println!("Byte: {:#X}", byte);
